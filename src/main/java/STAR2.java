4a733e78cb38d8c27a896d8745517413d87f8894
KimJiHeon <000224kjh@gmail.com>
2023-03-31 17:09:10 +0900

    학교 문제 풀이

---
+++ src/main/java/STAR2.java
@@ -0,0 +1,12 @@
+
+public class STAR2 {
+    public static void main(String[] args) {
+        int i;
+        String k=" ";
+
+        for(i=0; i<11; i++) {
+            System.out.println(k=k+"*");
+        }
+    }
+}
+// 문자열은 곱하기가 안된다는 사실을 깨닫고 이렇게 표현해봤습니다
